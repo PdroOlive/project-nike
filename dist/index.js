4aa844b5482b59343f4e24048ef6adcc595077d3
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-08-11 23:15:13 -0300

    changes

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("Hello World!!");
+const headerField = document.getElementById("header-field");
+let navField = document.querySelector("#header-field nav");
+headerField.onclick = (event) => {
+    const nikeBtn = (event === null || event === void 0 ? void 0 : event.target).closest(".nike-btn");
+    if (nikeBtn) {
+        if (navField.style.opacity === "1" && navField.style.visibility === "visible") {
+            navField.style.opacity = `0`;
+            navField.style.visibility = "hidden";
+        }
+        else {
+            navField.style.opacity = `1`;
+            navField.style.visibility = "visible";
+        }
+    }
+};
--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("Hello World!!")
+const headerField = document.getElementById("header-field");
+let navField = document.querySelector("#header-field nav") as HTMLElement;
+
+
+
+
+
+headerField!.onclick = (event:MouseEvent | null) =>
+{
+    
+    const nikeBtn = (event?.target as HTMLElement).closest(".nike-btn");
+    if(nikeBtn)
+    {        
+        if (navField!.style.opacity === "1" && navField!.style.visibility === "visible")
+        {
+            navField!.style.opacity = `0`;
+            navField!.style.visibility = "hidden";
+        }
+        else
+        {
+            navField!.style.opacity = `1`;
+            navField!.style.visibility = "visible";
+        }
+    }
+    
+}
