ef3a0f5fd74cacf87c1dd39054ca4d0506c00a39
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2024-08-12 19:29:01 -0300

    changes

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 "use strict";
 const headerField = document.getElementById("header-field");
 let navField = document.querySelector("#header-field nav");
+const mainField = document.getElementById("main-field");
+const modalField = document.getElementById("section-modal");
 headerField.onclick = (event) => {
     const nikeBtn = (event === null || event === void 0 ? void 0 : event.target).closest(".nike-btn");
     if (nikeBtn) {
@@ -12,5 +14,20 @@ headerField.onclick = (event) => {
             navField.style.opacity = `1`;
             navField.style.visibility = "visible";
         }
+    }
+};
+mainField.onclick = (event) => {
+    const addCartBtn = (event === null || event === void 0 ? void 0 : event.target).closest(".add-cart-btn");
+    if (addCartBtn) {
+        modalField.style.visibility = `visible`;
+        modalField.style.opacity = `1`;
+        modalField.style.transform = `scale(1)`;
+    }
+};
+modalField.onclick = (event) => {
+    console.log((event === null || event === void 0 ? void 0 : event.target).classList.contains("#section-modal"));
+    const closeModalBtn = (event === null || event === void 0 ? void 0 : event.target).closest(".close-modal-btn");
+    if (closeModalBtn) {
+        console.log("Fechar");
     }
 };
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const headerField = document.getElementById("header-field");
 let navField = document.querySelector("#header-field nav") as HTMLElement;
-
-
+const mainField = document.getElementById("main-field");
+const modalField = document.getElementById("section-modal");
 
 
 
@@ -23,4 +23,26 @@ headerField!.onclick = (event:MouseEvent | null) =>
         }
     }
     
+}
+
+
+mainField!.onclick = (event:MouseEvent | null) =>
+{
+    const addCartBtn = (event?.target as HTMLElement).closest(".add-cart-btn");
+    if(addCartBtn)
+    {
+        modalField!.style.visibility = `visible`;
+        modalField!.style.opacity = `1`;
+        modalField!.style.transform = `scale(1)`;
+    }
+}
+
+modalField!.onclick = (event:MouseEvent | null) =>
+{
+    console.log((event?.target as HTMLElement).classList.contains("#section-modal"))
+    const closeModalBtn = (event?.target as HTMLElement).closest(".close-modal-btn");
+    if(closeModalBtn)
+    {
+        console.log("Fechar")
+    }
 }
